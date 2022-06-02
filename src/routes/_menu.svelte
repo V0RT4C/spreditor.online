bc5689475801a7f2c0f7842ce526690c03f8b76d
Arai <arai@Arais-MacBook-Air.local>
2022-06-02 04:19:56 +0200

    Fixed menu more

--- src/routes/_menu.svelte
+++ src/routes/_menu.svelte
@@ -15,18 +15,18 @@
             />
         </div>
         <div class="lg:relative lg:top-[-10px] flex justify-end items-center px-2 gap-2 float-right">
-            <div class="w-40">
+            <div class="w-20">
                 <Select 
                     options={[{ name: 'Show all', value: 'all' }, { name: 'Show empty', value: 'empty' }]}
                     selectedOption={{ name: 'Show all', value: 'all' }}
                     on:change={(e) => dispatch('filter', e.detail)}
                 />
             </div>
-            <div class="relative w-40 text-gray-600 focus-within:text-gray-400">
+            <div class="relative w-28 text-gray-600 focus-within:text-gray-400">
                 <span class="absolute inset-y-0 left-0 flex items-center pl-2">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </span>
-                <input bind:value={searchStr} on:input={() => dispatch('search')} type="search" name="q" class="py-2 w-40 text-white text-xs bg-secondary rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900 border border-solid border-primary" placeholder="Search id..." autocomplete="off">
+                <input bind:value={searchStr} on:input={() => dispatch('search')} type="search" name="q" class="py-2 w-28 text-white text-xs bg-secondary rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900 border border-solid border-primary" placeholder="Search id..." autocomplete="off">
             </div>
         </div>
     {/if}
